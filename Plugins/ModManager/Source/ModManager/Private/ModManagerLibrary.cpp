c7e20f87ddc50086549963a20936a0efaa285878
JiRath <garethaguilar@gmail.com>
2024-12-25 00:15:10 -0500

    Begin implementing standalone plugin for implementing mods

---
+++ Plugins/ModManager/Source/ModManager/Private/ModManagerLibrary.cpp
@@ -0,0 +1,143 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ModManagerLibrary.h"
+
+#include "GameFeaturesSubsystem.h"
+#include "IPlatformFilePak.h"
+#include "Interfaces/IPluginManager.h"
+
+TArray<FString> UModManagerLibrary::GetPakFilesInDirectory(const FString& Path)
+{
+	TArray<FString> result;
+	FString Type = "*.pak";
+	IFileManager::Get().FindFilesRecursive(result, *Path, *Type, true,false);
+	return result;
+}
+
+bool UModManagerLibrary::LoadPakFile(const FString& Path)
+{
+	auto PlatformFile = GetOrAddPakPlatform();
+	FString PakName = FPaths::GetBaseFilename(Path);
+	FString RootPath = "/"+PakName+"/";
+
+	// Get platform path
+	FString InputPath = Path;
+	FPaths::MakePlatformFilename(InputPath);
+
+	// Retrieve mount point
+	FString MountPoint = "";
+	FPakFile* Pak = nullptr;
+	TRefCountPtr<FPakFile> PakFile = new FPakFile(PakPlatformFile, *Path, false);
+	Pak = PakFile.GetReference();
+	if (Pak->IsValid())
+	{
+		MountPoint = Pak->GetMountPoint();
+	}
+
+	FPackageName::RegisterMountPoint(RootPath, MountPoint+"Content/");
+	return PlatformFile->Mount(*InputPath, 0, *MountPoint);
+}
+
+bool UModManagerLibrary::UnloadPakFile(const FString& Path)
+{
+	auto PlatformFile = GetOrAddPakPlatform();
+	FString PakName = FPaths::GetBaseFilename(Path);
+	FString RootPath = "/"+PakName+"/";
+
+	// Retrieve mount point
+	FString MountPoint = "";
+	FPakFile* Pak = nullptr;
+	TRefCountPtr<FPakFile> PakFile = new FPakFile(PakPlatformFile, *Path, false);
+	Pak = PakFile.GetReference();
+	if (Pak->IsValid())
+	{
+		MountPoint = Pak->GetMountPoint();
+	}
+	
+	FPackageName::UnRegisterMountPoint(RootPath, MountPoint+"Content/");
+	return PlatformFile->Unmount(*Path);
+}
+
+TArray<FString> UModManagerLibrary::GetPluginsInDirectory(const FString& Path)
+{
+	TArray<FString> result;
+	FString Type = "*.uplugin";
+	IFileManager::Get().FindFilesRecursive(result, *Path, *Type, true,false);
+	return result;
+}
+
+bool UModManagerLibrary::LoadPlugin(UObject* WorldContext, const FString& Path, bool bIsGameFeature)
+{
+	auto World = GEngine->GetWorldFromContextObject(WorldContext, EGetWorldErrorMode::Assert);
+	auto PlatformFile = GetOrAddPakPlatform();
+	FString PluginDirectory = FPaths::GetPath(Path);
+	FString PluginName = FPaths::GetBaseFilename(Path);
+
+	// Register plugin
+	FText FailReason;
+	if (!IPluginManager::Get().AddToPluginsList(Path, &FailReason))
+		return false;
+	if (!IPluginManager::Get().MountExplicitlyLoadedPlugin(PluginName))
+		return false;
+
+	// Load pak files in plugin
+	TArray<FString> PakFiles = GetPakFilesInDirectory(PluginDirectory);
+	for (const auto& PakFile : PakFiles)
+	{
+		if (!LoadPakFile(PakFile))
+			return false;
+	}
+
+	// Register with game feature subsystem
+	if (bIsGameFeature)
+	{
+		if (auto GameFeaturesSubsystem = GEngine->GetEngineSubsystem<UGameFeaturesSubsystem>())
+		{
+			GameFeaturesSubsystem->LoadAndActivateGameFeaturePlugin(Path, FGameFeaturePluginLoadComplete());
+		}
+	}
+	return true;
+}
+
+bool UModManagerLibrary::UnloadPlugin(UObject* WorldContext, const FString& Path, bool bIsGameFeature)
+{
+	FString PluginDirectory = FPaths::GetPath(Path);
+	FString PluginName = FPaths::GetBaseFilename(Path);
+	
+	// Unload pak files in plugin
+	TArray<FString> PakFiles = GetPakFilesInDirectory(PluginDirectory);
+	for (const auto& PakFile : PakFiles)
+	{
+		if (!UnloadPakFile(PakFile))
+			return false;
+	}
+
+	// Unload game feature
+	if (bIsGameFeature)
+	{
+		if (auto GameFeaturesSubsystem = GEngine->GetEngineSubsystem<UGameFeaturesSubsystem>())
+		{
+			GameFeaturesSubsystem->UnloadGameFeaturePlugin(Path, FGameFeaturePluginUnloadComplete());
+		}
+	}
+	
+	return true;
+}
+
+FPakPlatformFile* UModManagerLibrary::GetOrAddPakPlatform()
+{
+	IPlatformFile* ExistingPlatformFile = FPlatformFileManager::Get().FindPlatformFile(TEXT("PakFile"));
+	if(ExistingPlatformFile)
+	{
+		PakPlatformFile = static_cast<FPakPlatformFile*>(ExistingPlatformFile);
+	}
+	else
+	{
+		PakPlatformFile = new FPakPlatformFile();
+		IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+		PakPlatformFile->Initialize(&PlatformFile, TEXT(""));
+		FPlatformFileManager::Get().SetPlatformFile(*PakPlatformFile);
+	}
+	return PakPlatformFile;
+}
---
+++ Plugins/ModManager/Source/ModManager/Public/ModManagerLibrary.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ModManagerLibrary.generated.h"
+
+class FPakPlatformFile;
+/**
+ * 
+ */
+UCLASS()
+class MODMANAGER_API UModManagerLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	// Retrieves all pak files recursively in the directory
+	UFUNCTION(BlueprintCallable, Category = "Mod Manager | Pak File Modding")
+	static TArray<FString> GetPakFilesInDirectory(const FString& Path);
+
+	// Mounts and registers a pak file for use.
+	UFUNCTION(BlueprintCallable, Category = "Mod Manager | Pak File Modding")
+	static bool LoadPakFile(const FString& Path);
+
+	// Unmounts a pak file
+	UFUNCTION(BlueprintCallable, Category = "Mod Manager | Pak File Modding")
+	static bool UnloadPakFile(const FString& Path);
+
+	// Retrieves all .uplugin files recursively in the directory
+	UFUNCTION(BlueprintCallable, Category = "Mod Manager | Plugin File Modding")
+	static TArray<FString> GetPluginsInDirectory(const FString& Path);
+
+	// Load a plugin for use
+	UFUNCTION(BlueprintCallable, meta=(WorldContext="WorldContext"), Category = "Mod Manager | Plugin File Modding")
+	static bool LoadPlugin(UObject* WorldContext, const FString& Path, bool bIsGameFeature);
+
+	// Unload a plugin
+	UFUNCTION(BlueprintCallable, meta=(WorldContext="WorldContext"), Category = "Mod Manager | Plugin File Modding")
+	static bool UnloadPlugin(UObject* WorldContext, const FString& Path, bool bIsGameFeature);
+protected:
+
+	static FPakPlatformFile* GetOrAddPakPlatform();
+	static FPakPlatformFile* PakPlatformFile;
+};
